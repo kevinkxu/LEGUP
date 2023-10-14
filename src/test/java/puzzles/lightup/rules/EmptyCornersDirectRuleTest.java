5990566b373fbbb3b77b1c591e740e0af1cb9b20
Kevin Xu <kevinkxu2013@gmail.com>
2023-10-13 23:01:24 -0400

    wip

--- src/test/java/puzzles/lightup/rules/EmptyCornersDirectRuleTest.java
+++ src/test/java/puzzles/lightup/rules/EmptyCornersDirectRuleTest.java
@@ -60,4 +60,40 @@ public class EmptyCornersDirectRuleTest {
             }
         }
     }
+
+    @Test
+    public void OneBlackTest() throws InvalidFileFormatException {
+        TestUtilities.importTestBoard("puzzles/lightup/rules/EmptyCornersDirectRule/EmptyCorners", lightUp);
+        TreeNode rootNode = lightUp.getTree().getRootNode();
+        TreeTransition transition = rootNode.getChildren().get(0);
+        transition.setRule(RULE);
+
+        //get board state
+        LightUpBoard board = (LightUpBoard) transition.getBoard();
+
+        //change the board's cells considering the EmptyCorners rule to empty
+        LightUpCell cell1 = board.getCell(2,2);
+        cell1.setData(LightUpCellType.EMPTY.value);
+        board.addModifiedData(cell1);
+
+        LightUpCell cell2 = board.getCell(0,2);
+        cell2.setData(LightUpCellType.EMPTY.value);
+        board.addModifiedData(cell2);
+
+        //confirm there is a logical following of the EmptyCorners rule
+        Assert.assertNull(RULE.checkRule(transition));
+
+        LightUpCell c;
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int j = 0; j < board.getWidth(); j++) {
+                c = board.getCell(j, i);
+                if ((i == 1 && j == 0) || (i == 0 && j == 2)){
+                    Assert.assertNotNull(RULE.checkRuleAt(transition, c));
+                }
+                else {
+                    Assert.assertNull(RULE.checkRuleAt(transition, c));
+                }
+            }
+        }
+    }
 }
